177238d5875c3e188052c5e173d16195d6780d16
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-09-19 09:25:41 +0200

    add status icon comp

---
+++ packages/client/src/components/status-icon.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { cn } from "@/utils/class-names"
+
+type Props = {
+  connected: boolean
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <i
+    class="mr-1.5 size-2 rounded-full bg-[#e38968]"
+    :class="cn(props.connected && 'bg-[#86bb71]')"
+  ></i>
+</template>
