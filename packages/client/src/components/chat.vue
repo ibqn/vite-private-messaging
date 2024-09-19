552eb2f50397e51ca478dfc619eb7c01e84c0872
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-09-19 11:13:33 +0200

    wip on chat

--- packages/client/src/components/chat.vue
+++ packages/client/src/components/chat.vue
@@ -1,5 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { User } from "server/src/types"
+import { ref } from "vue"
+
+const users = ref<User[]>([])
+const selectedUser = ref<User | null>(null)
+</script>
 
 <template>
-  <div>chat</div>
+  <div class="flex flex-row">
+    <div class="bg-[#3f0e40]">left</div>
+    <div>chat</div>
+  </div>
 </template>
