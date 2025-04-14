50f3399e97b473c9ae40f1fd8d0aaa65740f9450
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-04-14 09:17:15 +0200

    update Tailwind CSS integration and configuration files

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import path from "path"
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
   server: {
     proxy: {
       // Proxying websockets or socket.io: ws://localhost:5173/socket.io -> ws://localhost:5174/socket.io
